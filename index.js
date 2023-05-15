1fc2cb9e1f9444cc1fdaad192cbac527af86c49f
Aishu <85008182+chocolatecupcake2002@users.noreply.github.com>
2023-05-15 10:01:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dbConfig = require("./config/db.js");
+const auth = require("./middlewares/auth.js");
+const errors = require("./middlewares/errors.js");
+const { unless } = require("express-unless");
+const dotenv = require("dotenv");
+
+// create express app
+const app = express();
+dotenv.config();
+
+let MONGO_URI = "mongodb+srv://mc3453:<Sunny2002>@unify.dyadwbb.mongodb.net/?retryWrites=true&w=majority";
+// connectDB
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to Mongoose");
+  } catch (error) {
+    console.log("Cannot connect to Mongoose");
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Database Disconnected");
+});
+
+// middlewares
+auth.authenticateToken.unless = unless;
+app.use(
+  auth.authenticateToken.unless({
+    path: [
+      { url: "/auth/login", methods: ["POST"] },
+      { url: "/auth/signup", methods: ["POST"] },
+      { url: "/auth/otpLogin", methods: ["POST"] },
+      { url: "/auth/verifyOTP", methods: ["POST"] },
+    ],
+  })
+);
+
+app.use(express.json());
+
+// routes
+app.use("/", require("./routes/users"));
+
+// middleware for error responses
+app.use(errors.errorHandler);
+
+// listen for requests
+app.listen(process.env.port || 4000, function () {
+   console.log("API Connected!");
+});
+
+const port = process.env.PORT || 8000;
+const port = 8000;
+
+app.listen(port, "0.0.0.0", () => {
+  connect();
+  console.log(`API Connected on port ${port}...`);
+});
